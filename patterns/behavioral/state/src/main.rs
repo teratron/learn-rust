ff46c172865db708fa3d7a7e82c7e447095bfc86
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-05-05 13:32:37 +0300

    Update patterns.

---
+++ patterns/behavioral/state/src/main.rs
@@ -0,0 +1,67 @@
+//! Состояние — это поведенческий паттерн,
+//! позволяющий динамически изменять поведение объекта при смене его состояния.
+
+use cursive::{
+    Cursive,
+    event::Key,
+    view::Nameable,
+    views::{Dialog, TextView},
+};
+
+use player::Player;
+use state::{State, StoppedState};
+
+mod player;
+mod state;
+
+// Application context: a music player and a state.
+struct PlayerApplication {
+    player: Player,
+    state: Box<dyn State>,
+}
+
+fn main() {
+    let mut app = cursive::default();
+
+    app.set_user_data(PlayerApplication {
+        player: Player::default(),
+        state: Box::new(StoppedState),
+    });
+
+    app.add_layer(
+        Dialog::around(TextView::new("Press Play").with_name("Player Status"))
+            .title("Music Player")
+            .button("Play", |s| execute(s, "Play"))
+            .button("Stop", |s| execute(s, "Stop"))
+            .button("Prev", |s| execute(s, "Prev"))
+            .button("Next", |s| execute(s, "Next")),
+    );
+
+    app.add_global_callback(Key::Esc, |s| s.quit());
+
+    app.run();
+}
+
+fn execute(s: &mut Cursive, button: &'static str) {
+    let PlayerApplication {
+        mut player,
+        mut state,
+    } = s.take_user_data().unwrap();
+
+    let mut view = s.find_name::<TextView>("Player Status").unwrap();
+
+    // Here is how state mechanics work: the previous state
+    // executes an action and returns a new state.
+    // Each state has all 4 operations but reacts differently.
+    state = match button {
+        "Play" => state.play(&mut player),
+        "Stop" => state.stop(&mut player),
+        "Prev" => state.prev(&mut player),
+        "Next" => state.next(&mut player),
+        _ => unreachable!(),
+    };
+
+    state.render(&player, &mut view);
+
+    s.set_user_data(PlayerApplication { player, state });
+}
